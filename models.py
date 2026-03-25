1f8fafe9a8c4d3a73bf6a5f84e4cc53536f90b82
Userinum <tislam12345@mail.ru>
2026-03-25 14:26:47 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,13 +5,13 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    pwd = Column(String)
+    password = Column(String)
 
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True)
     title = Column(String)
-    text = Column(String)
-    st = Column(String)
-    pr = Column(Integer)
-    uid = Column(Integer, ForeignKey("users.id"))
+    description = Column(String)
+    status = Column(String)
+    priority = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id"))
