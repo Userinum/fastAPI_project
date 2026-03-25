3184265f1632fe12ae6c8bef7213592d0e28aca3
Userinum <tislam12345@mail.ru>
2026-03-25 14:27:41 +0300

    Update db.py

--- db.py
+++ db.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 eng = create_engine("sqlite:///./db.db", connect_args={"check_same_thread": False})
-Sess = sessionmaker(bind=eng)
+SessionLocal = sessionmaker(bind=eng)
 Base = declarative_base()
 
 def get_db():
-    db = Sess()
+    db = SessionLocal()
     try:
         yield db
     finally:
