8e099a8e069e024ed9518113d14622e3cce05366
Userinum <tislam12345@mail.ru>
2026-05-06 17:36:04 +0300

    Update test_root.py

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,4 +1,4 @@
 def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"ok": "yes"}е
+    assert response.json() == {"ok": "yes"}
