92560840d4978f8c1a9acd5dabdc1c03695d80b9
Userinum <tislam12345@mail.ru>
2026-05-06 21:43:51 +0300

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    DATABASE_URL = "sqlite:///./test.db"
+    DATABASE_URL = "sqlite:///./app.db"
 
 
 eng = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
